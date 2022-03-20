60fdac66c604393b46a2934c0b9c5c93a53a2705
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-03-20 13:26:43 +0100

    Move Login Panel component to the separate folder

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,21 +1,5 @@
-import styles from './Root.module.scss';
-import { FormField } from '@/components/molecules/FormField/FormField';
-import Button from '@/components/atoms/Button/Button';
-import { Routes, Route, Link } from 'react-router-dom';
-const LoginPanel = () => {
-  return (
-    <div className={styles.formWrapper}>
-      <form>
-        <h2 className={styles.signInTitle}>Sign in</h2>
-        <FormField id="username" placeholder="Enter username" label="Username" />
-        <FormField id="password" placeholder="Enter password" type="password" label="Password" />
-        <Link to="/dashboard">
-          <Button text="Login" />
-        </Link>
-      </form>
-    </div>
-  );
-};
+import { Routes, Route } from 'react-router-dom';
+import { LoginPanel } from './components/organisms/LoginPanel/LoginPanel';
 
 const Dashboard = () => {
   return <h2>dashboard</h2>;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import './styles/_index.scss';
 
 ReactDOM.render(
