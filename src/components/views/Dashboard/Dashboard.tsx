9216cfd1873272279abce4d4efca663da909f6f6
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-03-23 23:47:02 +0100

    Move manage table to a separate folder

--- src/components/views/Dashboard/Dashboard.tsx
+++ src/components/views/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
-import Button from '@/components/atoms/Button/Button';
 import { Header } from '@/components/atoms/Header/Header';
+import { ManageTable } from '@/components/organisms/ManageTable/ManageTable';
 import { Navigation } from '@/components/organisms/Navigation/Navigation';
 import styles from './Dashboard.module.scss';
 
@@ -8,15 +8,7 @@ export function Dashboard() {
     <div className={styles.dashboardWrapper}>
       <Navigation />
       <Header name="James" />
-      <main className={styles.tableWrapper}>
-        <div className={styles.table}>
-          <div className={styles.tableHeader}>
-            <input placeholder="Search" type="text" />
-            <Button text="Add admin" className={styles.addUser} />
-          </div>
-          <div className={styles.tableContent}></div>
-        </div>
-      </main>
+      <ManageTable />
     </div>
   );
 }
