76f63038b7cb9db7d20884baeb13a84b2282eab6
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-04-05 21:05:49 +0200

    change userrole enum to use strings

--- src/components/atoms/ManageTableItem/ManageTableItem.tsx
+++ src/components/atoms/ManageTableItem/ManageTableItem.tsx
@@ -2,7 +2,6 @@ import { User } from '@/types/User';
 import styles from './ManageTableItem.module.scss';
 import { Role } from '@/components/atoms/Role/Role';
 import { Button } from '@/components/atoms/Button/Button';
-
 import { FormField } from '@/components/molecules/FormField/FormField';
 import { Modal } from '@/components/organisms/UserModal/UserModal';
 import { useModal } from '@/hooks/useModal';
@@ -24,7 +23,7 @@ export function ManageTableItem({ userData }: ManageTabkeItemProps) {
       <Button text="Edit" onClick={handleOpenModal} />
       <Button text={userData?.isActive ? 'Deactivate' : 'Activate'} />
       <Modal
-        headerText={`Add admin`} //add logic to handle role
+        headerText={`Add ${userData.role}`}
         buttonText="Edit"
         isOpen={isModalOpen}
         handleClose={handleCloseModal}
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,7 +1,7 @@
 export enum UserRole {
-  Admin,
-  Manager,
-  Worker,
+  Admin = 'admin',
+  Manager = 'manager',
+  Worker = 'worker',
 }
 
 export type User = {
