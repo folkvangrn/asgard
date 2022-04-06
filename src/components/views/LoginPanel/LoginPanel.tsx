4661cd8bee941df075b5088513ebdbb8a86561eb
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-04-06 21:56:03 +0200

    Add form validation and submit via formik and yup libraries

--- src/components/views/LoginPanel/LoginPanel.tsx
+++ src/components/views/LoginPanel/LoginPanel.tsx
@@ -2,17 +2,52 @@ import { Button } from '@/components/atoms/Button/Button';
 import { FormField } from '@/components/molecules/FormField/FormField';
 import { Link } from 'react-router-dom';
 import styles from './LoginPanel.module.scss';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export function LoginPanel() {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required('Required'),
+      password: Yup.string().required('Required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className={styles.formWrapper}>
-      <form className={styles.loginForm}>
+      <form className={styles.loginForm} onSubmit={formik.handleSubmit}>
         <h2 className={styles.signInTitle}>Sign in</h2>
-        <FormField id="username" placeholder="Enter username" label="Username" />
-        <FormField id="password" placeholder="Enter password" type="password" label="Password" />
-        <Link to="/dashboard/admins">
-          <Button text="Login" />
-        </Link>
+        <FormField
+          id="username"
+          label="Username"
+          value={formik.values.username}
+          handleValueChange={formik.handleChange}
+          placeholder="Enter username"
+          isTouched={formik.touched.username}
+          error={formik.errors.username}
+          handleBlur={formik.handleBlur}
+        />
+        <FormField
+          id="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          handleValueChange={formik.handleChange}
+          placeholder="Enter password"
+          isTouched={formik.touched.password}
+          error={formik.errors.password}
+          handleBlur={formik.handleBlur}
+        />
+        {/* <Link to="/dashboard/admins"> */}
+        <Button text="Login" type="submit" />
+        {/* </Link> */}
       </form>
     </div>
   );
