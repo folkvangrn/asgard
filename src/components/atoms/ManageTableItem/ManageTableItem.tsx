914b566e9dea3295091b089a026c7dae7579233d
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-04-23 15:58:56 +0200

    Remove needless helper function in Role component

--- src/components/atoms/ManageTableItem/ManageTableItem.tsx
+++ src/components/atoms/ManageTableItem/ManageTableItem.tsx
@@ -1,7 +1,5 @@
-import { useModal } from '@/hooks/useModal';
 import { User } from '@/types/User';
 import { Role } from '@/components/atoms/Role/Role';
-import { Button } from '@/components/atoms/Button/Button';
 import styles from './ManageTableItem.module.scss';
 
 type ManageTabkeItemProps = {
