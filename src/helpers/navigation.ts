5d2324aa886ba9bc1f413c49661521d3e686ae7f
Jakub Lyczko <jakub.lyczko1@gmail.com>
2022-04-23 12:59:56 +0200

    Rename routes

--- src/helpers/navigation.ts
+++ src/helpers/navigation.ts
@@ -3,7 +3,7 @@ import { UserRole } from '@/types/User';
 export const getDefaultRoute = (role: UserRole | undefined) => {
   switch (role) {
     case UserRole.Worker:
-      return '/dashboard/requests';
+      return '/dashboard/activities';
     case UserRole.Manager:
       return '/dashboard/requests';
     case UserRole.Admin:
